0bdb561ee130c642f423626e6761b4496777b25e
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2023-01-21 15:16:27 +0700

    update script

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -3,9 +3,9 @@
 # Build Docker image
 docker build -t taqiyyaghazi/karsajobs:latest .
 
-# Log in to Docker Hub (or GitHub Package)
+# Log in to Docker Hub
 echo $PASSWORD_DOCKER_HUB | docker login -u taqiyyaghazi --password-stdin
 
-# Push image to Docker Hub (or GitHub Package)
+# Push image to Docker Hub
 docker push taqiyyaghazi/karsajobs:latest
 
